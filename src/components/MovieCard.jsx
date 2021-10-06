0c45140489b289dd6f67307f304515025619f98a
xafixav <xafiworld@gmail.com>
2021-10-06 18:15:19 -0300

    requirement 4 until 10 done

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import movies from '../data';
+import PropTypes from 'prop-types';
 
-function createImage(source) {
+function createImage(props) {
   return (
     <div className="movie-card">
+      <img alt="" src={ props.imagePath } className="movie-card-image" />
+      <h4 alt="" className="movie-card-title">{props.title}</h4>
+      <h5 alt="" className="movie-card-subtitle">{props.subtitle}</h5>
 
-      <img alt="" src={ source.imagePath } className="movie-card-image" />
     </div>
   );
 }
 
 class MovieCard extends React.Component {
   render() {
-    return movies.map((movie) => createImage(movie));
+    const { EachMovie } = this.props;
+    return (
+      createImage(EachMovie)
+    );
   }
 }
+
+MovieCard.propTypes = {
+  EachMovie: PropTypes.string.isRequired,
+};
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import MoviesCard from './MovieCard';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div>
-        <MoviesCard />
+        {movie.map((each) => <MovieCard key={ each.title } EachMovie={ each } />)}
       </div>
     );
   }
 }
+
+MovieList.propTypes = {
+  movie: PropTypes.string.isRequired,
+};
 
 export default MovieList;
