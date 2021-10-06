e04daa3a5ce806c9e422909d2cc0f523ce2cc493
xafixav <xafiworld@gmail.com>
2021-10-06 15:48:40 -0300

    requirement 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import './components/MovieCard';
-import './components/MovieList';
-import './components/Rating';
+import MovieList from './components/MovieList'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,19 @@
-// implement MovieCard component here
+import React from 'react';
+import movies from '../data';
+
+const createImage = (source) => {
+  return (
+    <div className="movie-card">
+
+      <img alt="" src={ source.imagePath } className="movie-card-image" />
+    </div>
+  );
+};
+
+class MovieCard extends React.Component {
+  render() {
+    return movies.map((movie) => createImage(movie));
+  }
+}
+
+export default MovieCard;
