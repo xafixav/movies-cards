6581f58a67aa5ecdff363d63ca3a2d57c6b95f80
xafixav <xafiworld@gmail.com>
2021-10-06 15:08:49 -0300

    requirement 1-2 and 15

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import './components/MovieCard';
+import './components/MovieList';
+import './components/Rating';
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */ }
+      {<Header />}
       Hora de codar! 🚀✍️👨‍💻👩‍💻💪
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header><h1>Movie Cards Library</h1></header>
+    );
+  }
+}
+
 // implement Header component here
+export default Header;
